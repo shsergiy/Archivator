bb6bc814535327827ca556133bc5b1b92e40f7f6
Sergiy Shlapak <s-shlapak@mail.ua>
2016-11-28 01:23:38 +0200

    7z Archivator

--- Archivation/Comand_7Z.cs
+++ Archivation/Comand_7Z.cs
@@ -6,21 +6,39 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using SevenZip.SDK;
 namespace Archivator_STP_Project_.Archivation
 {
     class Comand_7Z : IComand
     {
+        //todo
         public void ArchivateFile(List<string> Paths)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string outFile = "";
+                SaveFileDialog saveFile = new SaveFileDialog();
+                saveFile.Title = "Saving archive";
+                saveFile.FileName = "Archive (" + DateTime.Now.ToShortDateString() + ")";
+                saveFile.Filter = "Файл ZIP|*.zip";
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    outFile = saveFile.FileName;
+                }
+                else throw new Exception("Path warning");
+                SevenZip.SDK.Compress.LZMA.Encoder coder = new SevenZip.SDK.Compress.LZMA.Encoder();
+                FileStream input = new FileStream(Paths[1], FileMode.Open);
+                FileStream output = new FileStream(outFile, FileMode.Create);
+                coder.WriteCoderProperties(output);
+                output.Write(BitConverter.GetBytes(input.Length), 0, 8);
+                coder.Code(input, output, input.Length, -1, null);
+                output.Flush();
+                output.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(Convert.ToString(ex)); }
         }
-
-        public void ArchivateFile(string path)
-        {
-            throw new NotImplementedException();
-        }
-        //arch folder
+     
+        //NO
         public void ArchivateFolder(string Path)
         {
             throw new NotImplementedException();
@@ -49,17 +67,45 @@ namespace Archivator_STP_Project_.Archivation
             catch (Exception ex) { MessageBox.Show(Convert.ToString(ex)); }
         }
 
-      
+        //todo
         public void Dearchivate(string Path)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string outFile = "";
+                SaveFileDialog saveFile = new SaveFileDialog();
+                saveFile.Title = "Saving archive";
+                saveFile.FileName = "Archive (" + DateTime.Now.ToShortDateString() + ")";
+                saveFile.Filter = "Файл ZIP|*.zip";
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    outFile = saveFile.FileName;
+                }
+                SevenZip.SDK.Compress.LZMA.Decoder coder = new SevenZip.SDK.Compress.LZMA.Decoder();
+                FileStream input = new FileStream(Path, FileMode.Open);
+                FileStream output = new FileStream(outFile, FileMode.Create);
+
+  
+                byte[] properties = new byte[5];
+                input.Read(properties, 0, 5);
+
+                byte[] fileLengthBytes = new byte[8];
+                input.Read(fileLengthBytes, 0, 8);
+                long fileLength = BitConverter.ToInt64(fileLengthBytes, 0);
+
+                coder.SetDecoderProperties(properties);
+                coder.Code(input, output, input.Length, fileLength, null);
+                output.Flush();
+                output.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(Convert.ToString(ex)); }
         }
         //todo
         public void Fragmentation(string Path)
         {
             throw new NotImplementedException();
         }
-        //todo
+        //NO
         public void TestProblems(string Path)
         {
             throw new NotImplementedException();
